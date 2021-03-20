b009162fe963b4b05e2e1305b7fb72dc1a71deda
sttsoneva <79417305+sttsoneva@users.noreply.github.com>
2021-03-20 23:17:37 +0000

    Delete Reflection Exercises directory

--- Reflection Exercises/src/barracksWars/interfaces/Executable.java
+++
@@ -1,9 +0,0 @@
-package barracksWars.interfaces;
-
-import jdk.jshell.spi.ExecutionControl;
-
-public interface Executable {
-
-	String execute() throws ExecutionControl.NotImplementedException;
-
-}
--- Reflection Exercises/src/barracksWars/interfaces/Runnable.java
+++
@@ -1,7 +0,0 @@
-package barracksWars.interfaces;
-
-import jdk.jshell.spi.ExecutionControl;
-
-public interface Runnable {
-	void run() throws ExecutionControl.NotImplementedException;
-}
